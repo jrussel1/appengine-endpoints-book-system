e3f1cab37673b8c5db3247f795077dae8ea44cbf
jesse <jrussel1@macalester.edu>
2014-03-04 15:44:28 -0600

    initial commit

--- src/com/book/system/spi/Ids.java
+++ src/com/book/system/spi/Ids.java
@@ -13,14 +13,13 @@
  * limitations under the License.
  */
 
-package com.google.devrel.samples.ttt.spi;
+package com.book.system.spi;
 
 /**
  * Contains the client IDs for allowed clients consuming the tictactoe API.
  */
 public class Ids {
-  public static final String WEB_CLIENT_ID = "replace this with your web client application ID";
-  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
-  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String WEB_CLIENT_ID = "AIzaSyDnooOFM8zZl3ONm34XqZYkEDh_E0R86zE";
+  public static final String ANDROID_CLIENT_ID = "AIzaSyAqgc8yEfZMbm3ktDMyMI8XEzWOX5_QSaw";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
 }
--- war/js/render.js
+++ war/js/render.js
@@ -5,33 +5,25 @@
  * Provides methods for the utility methods needed to render the Google+
  * Sign-In button.
  *
- * @author dhermes@google.com (Danny Hermes)
  */
 
-/** google global namespace for Google projects. */
-var google = google || {};
+var book = book || {};
 
-/** devrel namespace for Google Developer Relations projects. */
-google.devrel = google.devrel || {};
+book.system = book.system || {};
 
-/** samples namespace for DevRel sample code. */
-google.devrel.samples = google.devrel.samples || {};
-
-/** TicTacToe namespace for this sample. */
-google.devrel.samples.ttt = google.devrel.samples.ttt || {};
 
 /**
  * Client ID of the application (from the APIs Console).
  * @type {string}
  */
-google.devrel.samples.ttt.CLIENT_ID =
-    'YOUR-CLIENT-ID';
+book.system.CLIENT_ID =
+    '615375088497-00o9ieitss4ikt6tgdaj6bki35e433iq.apps.googleusercontent.com';
 
 /**
  * Scopes used by the application.
  * @type {string}
  */
-google.devrel.samples.ttt.SCOPES =
+book.system.SCOPES =
     'https://www.googleapis.com/auth/userinfo.email ' +
     'https://www.googleapis.com/auth/plus.login';
 
@@ -50,7 +42,7 @@ google.devrel.samples.ttt.SCOPES =
  * @return {string} The email parsed from the claim set, else undefined
  *                  if one can't be parsed.
  */
-google.devrel.samples.ttt.getEmailFromIDToken = function(idToken) {
+book.system.getEmailFromIDToken = function(idToken) {
   if (typeof idToken !== 'string') {
     return;
   }
@@ -80,13 +72,12 @@ google.devrel.samples.ttt.getEmailFromIDToken = function(idToken) {
  * @param {Object} authResult The contents returned from the Google+
  *                            Sign In attempt.
  */
-google.devrel.samples.ttt.signinCallback = function(authResult) {
-  var tokenEmail = google.devrel.samples.ttt.getEmailFromIDToken(
+book.system.signinCallback = function(authResult) {
+  var tokenEmail = book.system.getEmailFromIDToken(
       authResult.id_token);
   if (authResult.access_token && tokenEmail) {
-    google.devrel.samples.ttt.init('//' + window.location.host + '/_ah/api',
+	  book.system.init('//' + window.location.host + '/_ah/api',
                                    tokenEmail);
-
     document.getElementById('signinButtonContainer').classList.remove(
         'visible');
     document.getElementById('signedInStatus').classList.add('visible');
@@ -108,18 +99,18 @@ google.devrel.samples.ttt.signinCallback = function(authResult) {
 /**
  * Renders the Google+ Sign-in button using auth parameters.
  */
-google.devrel.samples.ttt.render = function() {
+book.system.render = function() {
   gapi.signin.render('signinButton', {
-    'callback': google.devrel.samples.ttt.signinCallback,
-    'clientid': google.devrel.samples.ttt.CLIENT_ID,
+    'callback': book.system.signinCallback,
+    'clientid': book.system.CLIENT_ID,
     'cookiepolicy': 'single_host_origin',
     'requestvisibleactions': 'http://schemas.google.com/AddActivity',
-    'scope': google.devrel.samples.ttt.SCOPES
+    'scope': book.system.SCOPES
   });
 };
 // A quirk of the JSONP callback of the plusone client makes it so
 // our callback must exist as an element in window.
-window['google.devrel.samples.ttt.render'] = google.devrel.samples.ttt.render;
+window['book.system.render'] = book.system.render;
 
 // Recommended code to load Google+ JS library.
 (function() {
@@ -127,7 +118,7 @@ window['google.devrel.samples.ttt.render'] = google.devrel.samples.ttt.render;
   newScriptElement.type = 'text/javascript';
   newScriptElement.async = true;
   newScriptElement.src = 'https://apis.google.com/js/client:plusone.js' +
-                         '?onload=google.devrel.samples.ttt.render';
+                         '?onload=book.system.render';
   var firstScriptElement = document.getElementsByTagName('script')[0];
   firstScriptElement.parentNode.insertBefore(newScriptElement,
                                              firstScriptElement);
